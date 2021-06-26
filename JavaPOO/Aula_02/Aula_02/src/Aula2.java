a55ce0d4906fce6d5a62f25511ea67e1dee31016
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2021-06-26 18:19:54 -0300

    Organizando as dependência do projetos

--- JavaPOO/Aula_02/Aula_02/src/Aula2.java
+++ JavaPOO/Aula_02/Aula_02/src/Aula2.java
@@ -1,5 +1,4 @@
-public class Aula2 {
-    
+public class Aula2{  
     public static void main(String[] args){
         Caneta c1 = new Caneta();
         c1.cor = "Azul";
@@ -8,7 +7,6 @@ public class Aula2 {
         c1.carga = 55;
         //c1.status();
         //c1.rabiscar();
-
 
         Caneta c2 = new Caneta();
         c2.modelo = "Hostnet";
