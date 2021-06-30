13bd13da7ee278e19eb84710cb25331906c94435
Luis Rodrigo <74280280+NiziulLuizin@users.noreply.github.com>
2021-06-30 12:27:24 -0300

    Apredendo na prática Encapsulamento no java

---
+++ JavaPOO/Aula_06/Aula_06/src/Aula6.java
@@ -0,0 +1,8 @@
+public class Aula6 {
+    public  static void main(String[] args){
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.abrirMenu();
+    }
+}
